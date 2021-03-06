3b36a0191d2aff93be32240c75441704d1fc78a5
Yash Chhatre <yashchhatre@Yashs-MacBook-Air.local>
2021-03-05 18:12:35 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
 
-function App() {
-  return (
+
+const RecipeCard = ({title, calories, image, vendor}) => {
+  return(
+    <div>
+      <h1> Name: {title} </h1>
+      <h2>Vendor: {vendor}</h2>
+      <img src={image} alt=""/>
+      <p>Calories: {calories} </p>
+
+    </div>
+  )
+}
+const App = () => {
+
+
+  const [hits, setHits] = useState([]); 
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState(""); 
+  useEffect(() => {
+    getRecipes(); 
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [query])
+
+  const getRecipes = async() => {
+    console.log(query); 
+    const queryURL = `https://api.edamam.com/search?q=${query}&app_id=${process.env.REACT_APP_APP_ID}&app_key=${process.env.REACT_APP_APP_KEY}`; 
+    const data = await (await fetch(queryURL)).json(); 
+    console.log(process.env.REACT_APP_APP_ID); 
+    setHits(data.hits); 
+    console.log(data.hits); 
+  }
+
+
+  return (  
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={e => {
+        e.preventDefault(); 
+        setQuery(search); 
+        setSearch(""); 
+      }}> 
+        <input type="text" value={search} onChange={(e) => setSearch(e.target.value)}></input>
+        <button type="submit">Search</button>
+      </form>
+      {hits.map(hit => (
+        <RecipeCard key = {hit.recipe.shareAs} title = {hit.recipe.label}calories={hit.recipe.calories} vendor={hit.recipe.source} image={hit.recipe.image} />
+      ))}
     </div>
   );
 }
